c1ce6646ed527460a19045a403feef998ffe8e06
Ash Laidlaw and Brendan Grubb <brendan@>
2017-03-02 14:28:22 -0800

    Add new layout, add book, delete all buttons

--- app/app.php
+++ app/app.php
@@ -4,6 +4,14 @@
     require_once __DIR__.'/../src/Author.php';
     require_once __DIR__.'/../src/Book.php';
 
+    $server = 'mysql:host=localhost:8889;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     use Symfony\Component\Debug\Debug;
     Debug::enable();
 
@@ -13,8 +21,27 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
+        $books = Book::getAll();
+        $authors = Author::getAll();
 
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('books' => $books, 'authors' => $authors));
+    });
+
+    $app->post('/', function() use ($app) {
+        $new_book = new Book(filter_var($_POST['title'], FILTER_SANITIZE_MAGIC_QUOTES), filter_var($_POST['genre'], FILTER_SANITIZE_MAGIC_QUOTES));
+        $new_book->save();
+        $new_author = new Author($_POST['first_name'], $_POST['last_name']);
+        $new_author->save();
+        $new_book->addAuthor($new_author);
+
+        return $app->redirect('/');
+    });
+
+    $app->delete('/', function() use ($app) {
+        Book::deleteAll();
+        Author::deleteAll();
+
+        return $app->redirect('/');
     });
 
     return $app;
