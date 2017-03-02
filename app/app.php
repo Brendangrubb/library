0e3d6f0a0e700f527483bd78068680370a0a424f
Ash Laidlaw and Brendan Grubb <ash@>
2017-03-02 13:48:49 -0800

    Add index.html.twig, basic routing

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Author.php';
+    require_once __DIR__.'/../src/Book.php';
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
